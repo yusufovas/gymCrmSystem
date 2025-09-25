2fb3363c1439d89bfc2f1a8ca943f16ecce4496c
maftunayusupova <maftunayusufova8@gmail.com>
2025-09-25 19:20:23 +0500

    Remove star import from trainee test dao

--- src/main/java/code/facade/GymFacade.java
+++ src/main/java/code/facade/GymFacade.java
@@ -9,6 +9,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
+import java.awt.*;
+
 @Component
 @RequiredArgsConstructor
 public class GymFacade {
--- src/test/java/code/dao/TraineeDaoTest.java
+++ src/test/java/code/dao/TraineeDaoTest.java
@@ -9,7 +9,9 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class TraineeDaoTest {
 
