f12964426c21391a27650e4c34000000f77351c0
maftunayusupova <maftunayusufova8@gmail.com>
2025-10-03 02:55:33 +0500

    Replace * imports

--- src/main/java/code/model/Trainee.java
+++ src/main/java/code/model/Trainee.java
@@ -1,6 +1,17 @@
 package code.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
